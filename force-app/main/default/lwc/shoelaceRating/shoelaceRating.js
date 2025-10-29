b9a6860d1e0a2ef88b10ba1f74cd95f2c355938f
kumar <krupavenkatkumarbommana42566@gmail.com>
2025-10-29 08:43:00 +0000

    changed

--- force-app/main/default/lwc/shoelaceRating/shoelaceRating.js
+++ force-app/main/default/lwc/shoelaceRating/shoelaceRating.js
@@ -1,4 +1,8 @@
 import { LightningElement, api } from 'lwc';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+
+import SHOELACE_JS from '@salesforce/resourceUrl/shoelace_js';
+import SHOELACE_CSS from '@salesforce/resourceUrl/shoelace_css';
 
 export default class ShoelaceRating extends LightningElement {
     @api max = 5;
@@ -7,18 +11,45 @@ export default class ShoelaceRating extends LightningElement {
     @api readOnly = false;
 
     ratingEl;
+    loadingPromise;
 
     renderedCallback() {
-        if (!this.ratingEl) {
-            this.createRating();
+        if (this.ratingEl) {
+            this.syncAttributes();
+            return;
         }
-        this.syncAttributes();
+
+        if (!this.loadingPromise) {
+            this.loadingPromise = Promise.all([
+                loadScript(this, SHOELACE_JS),
+                loadStyle(this, SHOELACE_CSS)
+            ])
+                .then(() => {
+                    this.createRating();
+                    this.syncAttributes();
+                })
+                .catch((error) => {
+                    // Surface the failure but allow the form to stay usable
+                    // eslint-disable-next-line no-console
+                    console.error('Failed to load rating control', error);
+                });
+        }
     }
 
     createRating() {
+        if (this.ratingEl) {
+            return;
+        }
+
+        const host = this.template.querySelector('.shoelace-rating-host');
+        if (!host) {
+            return;
+        }
+
         this.ratingEl = document.createElement('sl-rating');
         this.ratingEl.classList.add('shoelace-rating');
-        this.ratingEl.setAttribute('style', '--symbol-size:1.5rem; --symbol-spacing:0.25rem;');
+        this.ratingEl.style.setProperty('--symbol-size', '1.5rem');
+        this.ratingEl.style.setProperty('--symbol-spacing', '0.25rem');
         this.ratingEl.addEventListener('sl-change', (event) => {
             this.value = event.target.value;
             this.dispatchEvent(
@@ -27,17 +58,17 @@ export default class ShoelaceRating extends LightningElement {
                 })
             );
         });
-        this.template.querySelector('.shoelace-rating-host').appendChild(this.ratingEl);
-        this.syncAttributes();
+
+        host.appendChild(this.ratingEl);
     }
 
     syncAttributes() {
         if (!this.ratingEl) {
             return;
         }
-        this.ratingEl.max = this.max;
-        this.ratingEl.precision = this.precision;
-        this.ratingEl.value = this.value;
-        this.ratingEl.readonly = this.readOnly;
+        this.ratingEl.max = Number(this.max) || 5;
+        this.ratingEl.precision = Number(this.precision) || 1;
+        this.ratingEl.value = Number(this.value) || 0;
+        this.ratingEl.readonly = this.readOnly === true || this.readOnly === 'true';
     }
 }
